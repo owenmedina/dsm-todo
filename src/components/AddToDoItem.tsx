bafc48b4c0e7fb1bb4d2056fd56a591c3106447a
Owen Medina <owensmedi@gmail.com>
2021-05-06 20:25:03 -0700

    Add new item to items array in memory

--- src/components/AddToDoItem.tsx
+++ src/components/AddToDoItem.tsx
@@ -1,8 +1,28 @@
 import Input from "@material-ui/core/Input"
+import { useState } from 'react';
 
-const AddToDoItem: React.FC = () => {
-    return (<form>
-        <Input placeholder="New item" disableUnderline={true}/>
+import Item from '../types/Item';
+
+interface Props {
+    onAdd: (item: Item) => void;
+}
+
+const AddToDoItem: React.FC<Props> = ({onAdd}) => {
+    const defaultItem = new Item('');
+    const [newItem, setNewItem] = useState(defaultItem);
+
+    const changeHandler = (ev: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = ev.target;
+        setNewItem(new Item(value));
+    };
+
+    const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        onAdd(newItem);
+        setNewItem(new Item(''));
+    };
+    return (<form onSubmit={(submitHandler)}>
+        <Input value={newItem.label} placeholder="New item" disableUnderline={true} onChange={changeHandler}/>
     </form>);
 };
 
--- src/components/ToDoBody.tsx
+++ src/components/ToDoBody.tsx
@@ -1,16 +1,24 @@
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
+import { useState } from 'react';
 
 import AddToDoItem from "./AddToDoItem";
-
+import Item from '../types/Item';
 
 const ToDoBody: React.FC = () => {
+    const defaultItems: Array<Item> = [];
+    const [items, setItems] = useState(defaultItems);
+
+    const addItem = (item: Item) => {
+        setItems((prevItems) => [...prevItems, item]);
+        console.log("Added an item");
+    }
     return (<List>
         <ListItem >
             ToDoListItems
         </ListItem>
         <ListItem >
-            <AddToDoItem />
+            <AddToDoItem onAdd={addItem}/>
         </ListItem>
     </List>);
 };
